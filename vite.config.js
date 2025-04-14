8e688e186e7bad5ea3339de48eca37e436c6b8e0
Samarth Patel <samarthnimeshkumarpatel@gmail.com>
2025-04-15 00:38:53 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://vue-jobs-ihbo.onrender.com/",
+        target: "https://vue-jobs-ihbo.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
