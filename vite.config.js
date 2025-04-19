7e04d2d8363f047e384f9a3ace2f422a97ed0550
Samarth Patel <samarthnimeshkumarpatel@gmail.com>
2025-04-19 18:30:08 +0530

    new file:   backend 	modified:   vite.config.js 	modified:   backend (modified content)

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://vue-jobs-ihbo.onrender.com/",
+        target: "https://vue-jobs-backend.onrender.com/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
